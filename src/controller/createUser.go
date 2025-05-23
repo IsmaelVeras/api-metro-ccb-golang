99c4c74655e6fb7cd90af3a36b64a741a89d3a9f
Ismael Veras <ismaelverass@outlook.com>
2025-05-22 22:32:30 -0300

    Criando objetos do usuario

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,25 @@
 package controller
 
 import (
+	"fmt"
+
 	rest_err "github.com/IsmaelVeras/api-golang-crud/src/configuration"
+	"github.com/IsmaelVeras/api-golang-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Invalid JSON body: error%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println("Nome:", userRequest.Name)
+	fmt.Println("Email:", userRequest.Email)
+	fmt.Println("Password:", userRequest.Password)
+	fmt.Println("Age:", userRequest.Age)
 }
---
+++ src/controller/model/response/user_response.go
@@ -0,0 +1,8 @@
+package response
+
+type UserResponse struct {
+	ID    string `json:"id"`
+	Email string `json:"email" binding:"required,email"`
+	Name  string `json:"name" binding:"required"`
+	Age   int8   `json:"age" binding:"required"`
+}
